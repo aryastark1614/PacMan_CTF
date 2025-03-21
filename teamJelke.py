0e17fc75f974d0c3522e3839a4e5e335890dacb6
Jelke Schröder <74502191+Jelkee@users.noreply.github.com>
2025-03-21 11:31:12 +0100

    Uh oh i'm gonna get beaten up

--- teamJelke.py
+++ teamJelke.py
@@ -25,7 +25,7 @@ from game import Directions
 #################
 
 def createTeam(firstIndex, secondIndex, isRed,
-               first = 'AttackingAgent', second = 'DefendingAgent'):
+               first = 'AttackingDefenderAgentTop', second = 'AttackingDefenderAgentTop'):
   """
   This function should return a list of two agents that will form the
   team, initialized using firstIndex and secondIndex as their agent
@@ -48,18 +48,155 @@ def createTeam(firstIndex, secondIndex, isRed,
 # Agents #
 ##########
 
-class AttackingAgent(CaptureAgent):
+class AttackingDefenderAgentTop(CaptureAgent):
 
   def registerInitialState(self, gameState):
     
-    self.simulation_time = 0.1
-    self.max_depth = 6
+    self.simulation_time = 0.2
+    self.max_depth = 10
     
     CaptureAgent.registerInitialState(self, gameState)
     
     self.start = gameState.getAgentPosition(self.index)
     self.food_carrying = 0
-    self.max_food_to_carry = 5
+    self.max_food_to_carry = 1
+
+
+  def chooseAction(self, gameState):
+    start_time = time.time()
+    
+    if not gameState.getAgentState(self.index).isPacman:
+      self.food_carrying = 0
+    
+    actions = gameState.getLegalActions(self.index)
+    simulations = {action: 0 for action in actions}
+    scores = {action: 0 for action in actions}
+
+    for action in actions:
+        next_state = gameState.generateSuccessor(self.index, action)
+        if len(self.getFood(gameState).asList()) > len(self.getFood(next_state).asList()):
+            scores[action] += 5000
+    
+    while time.time() - start_time < self.simulation_time:
+      action = random.choice(actions)
+      next_state = gameState.generateSuccessor(self.index, action)
+      
+      sim_food_carrying = self.food_carrying
+      if len(self.getFood(gameState).asList()) > len(self.getFood(next_state).asList()):
+          sim_food_carrying += 1
+      
+      reward = self.simulate(next_state, depth=0, food_carrying=sim_food_carrying)
+      simulations[action] += 1
+      scores[action] += reward
+    
+    best_action = max(actions, key=lambda a: scores[a] / (simulations[a] + 1e-6))
+
+    successor = gameState.generateSuccessor(self.index, best_action)
+    if len(self.getFood(gameState).asList()) > len(self.getFood(successor).asList()):
+        self.food_carrying += 1
+    
+    print(f"Best action: {best_action}, Score: {scores[best_action] / (simulations[best_action] + 1e-6)}")
+    return best_action
+  
+  def simulate(self, gameState, depth, food_carrying):
+        if depth >= self.max_depth or gameState.isOver():
+            return self.evaluate(gameState, food_carrying)# * (0.9 ** depth)
+
+        actions = gameState.getLegalActions(self.index)
+        
+        if not actions:
+            return self.evaluate(gameState, food_carrying)
+        
+        action_scores = []
+        for action in actions:
+            next_state = gameState.generateSuccessor(self.index, action)
+            
+            sim_food_carrying = food_carrying
+            if len(self.getFood(gameState).asList()) > len(self.getFood(next_state).asList()):
+                sim_food_carrying += 1
+                
+            score = self.evaluate_action(gameState, action, food_carrying, sim_food_carrying)
+            action_scores.append((action, score, sim_food_carrying))
+        
+        total_score = sum(max(1, score) for _, score, _ in action_scores)
+        probabilities = [max(1, score) / total_score for _, score, _ in action_scores]
+        
+        chosen_idx = random.choices(range(len(action_scores)), weights=probabilities)[0]
+        action, _, new_food_carrying = action_scores[chosen_idx]
+        
+        next_state = gameState.generateSuccessor(self.index, action)
+    
+        all_indexes = [0, 1, 2, 3]
+        all_indexes.remove(self.index)
+        
+        for agent in all_indexes:
+            agent_actions = next_state.getLegalActions(agent)
+            if agent_actions:
+                agent_action = random.choice(agent_actions)
+                next_state = next_state.generateSuccessor(agent, agent_action)
+        return self.simulate(next_state, depth + 1, new_food_carrying)
+      
+  def evaluate_action(self, gameState, action, current_food_carrying, next_food_carrying):
+      successor = gameState.generateSuccessor(self.index, action)
+      
+      if next_food_carrying > current_food_carrying:
+          return 1000
+      
+      return self.evaluate(successor, next_food_carrying)
+      
+  def evaluate(self, gameState, food_carrying=None):
+
+    if food_carrying is None:
+        food_carrying = self.food_carrying
+        
+    position = gameState.getAgentPosition(self.index)
+    #food_list = self.getFood(gameState).asList()
+    
+    score = 0
+    
+    if food_carrying >= self.max_food_to_carry:
+      dist_to_start = self.getMazeDistance(position, self.start)
+      return -(10 * dist_to_start)
+
+    opp = self.getOpponents(gameState)
+    enemies = [gameState.getAgentPosition(i) for i in opp]
+    enemy_pacmans = [en for en in opp if gameState.getAgentState(en).isPacman]
+
+    for en in self.getOpponents(gameState):
+      if self.red:
+        # print(f"{gameState.getAgentPosition(en)[0]}")
+        # print(position[0])
+        if gameState.getAgentPosition(en)[0] == 30 and position[0] != 1:
+          score += 1000
+      else:
+        if gameState.getAgentPosition(en)[0] == 1 and position[0] != 30:
+          score += 1000
+    if (enemy_pacmans):
+      closest_pacman = min(self.getMazeDistance(position, gameState.getAgentPosition(ep)) for ep in enemy_pacmans)
+      score -= 5 * closest_pacman
+    else:
+      closest_enemy = min(self.getMazeDistance(position, enemy) for enemy in enemies)
+      score -= closest_enemy
+    
+    if gameState.getAgentState(self.index).isPacman:
+      score -= 100
+            
+    return score
+
+
+
+class AttackingDefenderAgentBottom(CaptureAgent):
+
+  def registerInitialState(self, gameState):
+    
+    self.simulation_time = 0.1
+    self.max_depth = 7
+    
+    CaptureAgent.registerInitialState(self, gameState)
+    
+    self.start = gameState.getAgentPosition(self.index)
+    self.food_carrying = 0
+    self.max_food_to_carry = 1
 
 
   def chooseAction(self, gameState):
@@ -127,7 +264,16 @@ class AttackingAgent(CaptureAgent):
         action, _, new_food_carrying = action_scores[chosen_idx]
         
         next_state = gameState.generateSuccessor(self.index, action)
-        return self.simulate(next_state, depth + 1, new_food_carrying)
+        
+        opponent_indices = self.getOpponents(gameState)
+    
+        for opponent in opponent_indices:
+            opponent_actions = next_state.getLegalActions(opponent)
+            if opponent_actions:
+                opponent_action = random.choice(opponent_actions)  # Assume random opponent behavior
+                next_state = next_state.generateSuccessor(opponent, opponent_action)
+
+        return self.simulate(next_state, depth + 2, new_food_carrying)
       
   def evaluate_action(self, gameState, action, current_food_carrying, next_food_carrying):
       successor = gameState.generateSuccessor(self.index, action)
@@ -151,113 +297,33 @@ class AttackingAgent(CaptureAgent):
       dist_to_start = self.getMazeDistance(position, self.start)
       return 10000 - (10 * dist_to_start)
     
-    score += 2000 * food_carrying
+    # score += 2000 * food_carrying
     
-    if len(food_list) > 2:
-      min_food_dist = min(self.getMazeDistance(position, food) for food in food_list)
-      score -= 10 * min_food_dist
+    # if len(food_list) > 2:
+    #   min_food_dist = min(self.getMazeDistance(position, food) for food in food_list)
+    #   score -= 10 * min_food_dist
     
+    # else:
+    #   score += 1000 - self.getMazeDistance(position, self.start)
+      
+    opp = self.getOpponents(gameState)
+    enemies = [gameState.getAgentPosition(i) for i in opp]
+    enemy_pacmans = [en for en in opp if gameState.getAgentState(en).isPacman]
+
+    for en in self.getOpponents(gameState):
+      if self.red:
+        if gameState.getAgentPosition(en)[0] == 30 and position[0] != 0:
+          score += 1000
+      else:
+        if gameState.getAgentPosition(en)[0] == 0 and position[0] != 30:
+          score += 1000
+    if (enemy_pacmans):
+      closest_pacman = min(self.getMazeDistance(position, gameState.getAgentPosition(ep)) for ep in enemy_pacmans)
+      score -= 3 * closest_pacman
     else:
-      score += 1000 - self.getMazeDistance(position, self.start)
-      
-    enemies = [gameState.getAgentState(i) for i in self.getOpponents(gameState)]
-
-    if enemies:
-      ghost_dists = [self.getMazeDistance(position, enemy.getPosition()) for enemy in enemies]
-      if min(ghost_dists) <= 2:
-        score -= 1000 * food_carrying
-      elif min(ghost_dists) <= 5:
-        score -= 100 * food_carrying
-        
-        
-    if gameState.getAgentState(self.index).isPacman:
-      score += 100
-            
-    return score
-
-
-
-
-
-class DefendingAgent(CaptureAgent):
-
-  def registerInitialState(self, gameState):
-      
-    self.simulation_time = 0.1
-    self.max_depth = 10
+      closest_enemy = min(self.getMazeDistance(position, enemy) for enemy in enemies)
+      score -= closest_enemy
     
-    CaptureAgent.registerInitialState(self, gameState)
-
-  def chooseAction(self, gameState):
-    
-    start_time = time.time()
-    actions = gameState.getLegalActions(self.index)
-    simulations = {action: 0 for action in actions}
-    scores = {action: 0 for action in actions}
-    
-    while start_time < self.simulation_time:
-      action = random.choice(actions)
-      next_state = gameState.generateSuccessor(self.index, action)
-      
-      reward = self.simulate(next_state, depth=0)
-      simulations[action] += 1
-      scores[action] += reward
-    
-    best_action = max(actions, key=lambda a: scores[a] / (simulations[a] + 1e-6))
-    
-    
-    print(f"Best action: {best_action}, Score: {scores[best_action] / (simulations[best_action] + 1e-6)}")
-    return best_action
-  
-  def simulate(self, gameState, depth):
-    
-    if depth >= self.max_depth or gameState.isOver():
-        return self.evaluate(gameState) * (0.9 ** depth)
-
-    actions = gameState.getLegalActions(self.index)
-    
-    if not actions:
-        return self.evaluate(gameState)
-    
-    action_scores = []
-    for action in actions:
-        next_state = gameState.generateSuccessor(self.index, action)
-            
-        score = self.evaluate_action(gameState, action)
-        action_scores.append((action, score))
-    
-    total_score = sum(max(1, score) for _, score, _ in action_scores)
-    probabilities = [max(1, score) / total_score for _, score, _ in action_scores]
-    
-    chosen_idx = random.choices(range(len(action_scores)), weights=probabilities)[0]
-    action, _, new_food_carrying = action_scores[chosen_idx]
-    
-    next_state = gameState.generateSuccessor(self.index, action)
-    return self.simulate(next_state, depth + 1, new_food_carrying)
-  
-
-  def evaluate_action(self, gameState, action):
-      successor = gameState.generateSuccessor(self.index, action)
-      
-      return self.evaluate(successor)
-    
-  def evaluate(self, gameState):
-
-    position = gameState.getAgentPosition(self.index)
-    
-    score = 0   
-    score += 1000 - self.getMazeDistance(position, self.start)
-      
-    enemies = [gameState.getAgentState(i) for i in self.getOpponents(gameState)]
-
-    if enemies:
-      ghost_dists = [self.getMazeDistance(position, enemy.getPosition()) for enemy in enemies]
-      if min(ghost_dists) <= 2:
-        score += 1000
-      elif min(ghost_dists) <= 5:
-        score += 100
-        
-    #penalty for being on the opponents side
     if gameState.getAgentState(self.index).isPacman:
       score -= 100
             
