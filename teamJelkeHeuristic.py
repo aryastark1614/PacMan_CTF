0680e47a74f21a2a3a91de15661f6ffcdd72beef
Jelke Schröder <74502191+Jelkee@users.noreply.github.com>
2025-03-26 13:11:05 +0100

    Heuristic actually good??

--- teamJelkeHeuristic.py
+++ teamJelkeHeuristic.py
@@ -25,7 +25,7 @@ from game import Directions
 #################
 
 def createTeam(firstIndex, secondIndex, isRed,
-               first = 'AttackingDefenderAgentTop', second = 'AttackingDefenderAgentTop'):
+               first = 'AttackingDefenderAgentTop', second = 'AttackingDefenderAgentBottom'):
   """
   This function should return a list of two agents that will form the
   team, initialized using firstIndex and secondIndex as their agent
@@ -66,14 +66,10 @@ class AttackingDefenderAgentTop(CaptureAgent):
       self.food_carrying = 0
     
     actions = gameState.getLegalActions(self.index)
-    print(actions)
-    simulations = {action: 0 for action in actions}
+
     scores = {action: 0 for action in actions}
 
-    for action in actions:
-        next_state = gameState.generateSuccessor(self.index, action)
-        if len(self.getFood(gameState).asList()) > len(self.getFood(next_state).asList()):
-            scores[action] += 1000
+
     
     for action in actions:
         
@@ -81,11 +77,14 @@ class AttackingDefenderAgentTop(CaptureAgent):
       sim_food_carrying = self.food_carrying
       if len(self.getFood(gameState).asList()) > len(self.getFood(next_state).asList()):
           sim_food_carrying += 1
+          self.food_carrying += 1
+          scores[action] += 10000
       
       reward = self.evaluate(next_state, food_carrying=sim_food_carrying)
       scores[action] += reward
 
     best_action = max(actions, key=lambda a: scores[a])
+    print(scores[best_action])
     
     if best_action not in actions:
       return random.choice(actions)
@@ -104,19 +103,27 @@ class AttackingDefenderAgentTop(CaptureAgent):
     
     if food_carrying >= self.max_food_to_carry:
       dist_to_start = self.getMazeDistance(position, self.start)
-      return -(10 * dist_to_start)
+      return 100000-(10 * dist_to_start)
 
     opp = self.getOpponents(gameState)
     enemies = [gameState.getAgentPosition(i) for i in opp]
     enemy_pacmans = [en for en in opp if gameState.getAgentState(en).isPacman]
 
-    # for en in self.getOpponents(gameState):
-    #   if self.red:
-    #     if gameState.getAgentPosition(en)[0] == 30 and position[0] != 1:
-    #       score += 100
-    #   else:
-    #     if gameState.getAgentPosition(en)[0] == 1 and position[0] != 30:
-    #       score += 100
+    for en in self.getOpponents(gameState):
+      if self.red:
+        if gameState.getAgentPosition(en)[0] == 30 and position[0] != 1:
+          score += 1000
+      else:
+        if gameState.getAgentPosition(en)[0] == 1 and position[0] != 30:
+          score += 1000
+          
+    for en in self.getOpponents(gameState):
+      if self.red:
+        if gameState.getAgentPosition(en)[0] != 30 and position[0] == 1:
+          score -= 1000
+      else:
+        if gameState.getAgentPosition(en)[0] != 1 and position[0] == 30:
+          score -= 1000
           
     if enemy_pacmans:
       closest_pacman = min(enemy_pacmans, key=lambda ep: self.getMazeDistance(position, gameState.getAgentPosition(ep)))
@@ -137,12 +144,134 @@ class AttackingDefenderAgentTop(CaptureAgent):
       print("CLOSE TO FOOD")
       score -= 20 * distance_to_closest_food
     
-    # capsules = self.getCapsules(gameState)
-    # if capsules:
-    #     capsule = capsules[0]
-    #     closest_enemy_to_capsule = min(self.getMazeDistance(capsule, enemy) for enemy in enemies)
-    #     distance_to_capsule = self.getMazeDistance(position, capsule)
-    #     if distance_to_capsule < closest_enemy_to_capsule:
-    #         score -= 30 * distance_to_capsule
+    capsules = self.getCapsules(gameState)
+    if capsules:
+        capsule = capsules[0]
+        closest_enemy_to_capsule = min(self.getMazeDistance(capsule, enemy) for enemy in enemies)
+        distance_to_capsule = self.getMazeDistance(position, capsule)
+        if distance_to_capsule < closest_enemy_to_capsule:
+            score -= 10 * distance_to_capsule
+            
+    #Try to have one agent go to the other side of the board
+    get_teammate = self.getTeam(gameState)
+    teammate = get_teammate[0] if get_teammate[0] != self.index else get_teammate[1]
+    teammate_position = gameState.getAgentPosition(teammate)
+    score += self.getMazeDistance(position, teammate_position)
     
+    
+
+    return score
+
+class AttackingDefenderAgentBottom(CaptureAgent):
+
+  def registerInitialState(self, gameState):
+    
+    self.simulation_time = 0.01
+    CaptureAgent.registerInitialState(self, gameState)
+    
+    self.start = gameState.getAgentPosition(self.index)
+    self.food_carrying = 0
+    self.max_food_to_carry = 1
+
+  def chooseAction(self, gameState):
+    start_time = time.time()
+    
+    if not gameState.getAgentState(self.index).isPacman:
+      self.food_carrying = 0
+    
+    actions = gameState.getLegalActions(self.index)
+
+    scores = {action: 0 for action in actions}
+
+
+    
+    for action in actions:
+        
+      next_state = gameState.generateSuccessor(self.index, action)
+      sim_food_carrying = self.food_carrying
+      if len(self.getFood(gameState).asList()) > len(self.getFood(next_state).asList()):
+          sim_food_carrying += 1
+          self.food_carrying += 1
+          scores[action] += 10000
+      
+      reward = self.evaluate(next_state, food_carrying=sim_food_carrying)
+      scores[action] += reward
+
+    best_action = max(actions, key=lambda a: scores[a])
+    print(scores[best_action])
+    
+    if best_action not in actions:
+      return random.choice(actions)
+    return best_action
+  
+  def evaluate(self, gameState, food_carrying=None):
+
+    if food_carrying is None:
+        food_carrying = self.food_carrying
+        
+    position = gameState.getAgentPosition(self.index)
+    food_list = self.getFood(gameState).asList()
+    
+    score = 0
+    
+    
+    if food_carrying >= self.max_food_to_carry:
+      dist_to_start = self.getMazeDistance(position, self.start)
+      return 100000-(10 * dist_to_start)
+
+    opp = self.getOpponents(gameState)
+    enemies = [gameState.getAgentPosition(i) for i in opp]
+    enemy_pacmans = [en for en in opp if gameState.getAgentState(en).isPacman]
+
+    for en in self.getOpponents(gameState):
+      if self.red:
+        if gameState.getAgentPosition(en)[0] == 30 and position[0] != 1:
+          score += 1000
+      else:
+        if gameState.getAgentPosition(en)[0] == 1 and position[0] != 30:
+          score += 1000
+          
+    for en in self.getOpponents(gameState):
+      if self.red:
+        if gameState.getAgentPosition(en)[0] != 30 and position[0] == 1:
+          score -= 1000
+      else:
+        if gameState.getAgentPosition(en)[0] != 1 and position[0] == 30:
+          score -= 1000
+          
+    if enemy_pacmans:
+      closest_pacman = min(enemy_pacmans, key=lambda ep: self.getMazeDistance(position, gameState.getAgentPosition(ep)))
+      distance_to_pacman = self.getMazeDistance(position, gameState.getAgentPosition(closest_pacman))
+      score += 150 -  3 * distance_to_pacman
+
+    else:
+      closest_enemy = min(self.getMazeDistance(position, enemy) for enemy in enemies)
+      score += 50 - closest_enemy
+    
+    if gameState.getAgentState(self.index).isPacman:
+      score -= 10
+      
+    closest_food = min(food_list, key=lambda food: self.getMazeDistance(position, food))
+    closest_enemy_to_closest_food = min(self.getMazeDistance(closest_food, enemy) for enemy in enemies)
+    distance_to_closest_food = self.getMazeDistance(position, closest_food)
+    if 2 * distance_to_closest_food < closest_enemy_to_closest_food:
+      print("CLOSE TO FOOD")
+      score -= 20 * distance_to_closest_food
+    
+    capsules = self.getCapsules(gameState)
+    if capsules:
+        capsule = capsules[0]
+        closest_enemy_to_capsule = min(self.getMazeDistance(capsule, enemy) for enemy in enemies)
+        distance_to_capsule = self.getMazeDistance(position, capsule)
+        if distance_to_capsule < closest_enemy_to_capsule:
+            score -= 10 * distance_to_capsule
+            
+    #Try to have one agent go to the other side of the board
+    # get_teammate = self.getTeam(gameState)
+    # teammate = get_teammate[0] if get_teammate[0] != self.index else get_teammate[1]
+    # teammate_position = gameState.getAgentPosition(teammate)
+    # score += self.getMazeDistance(position, teammate_position)
+    
+    
+
     return score
